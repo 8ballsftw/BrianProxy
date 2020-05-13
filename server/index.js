aebc69c0abd87f7db72ea1c14e1323014baa7ec5
Brian de Regt <brian.deregt@gmail.com>
2020-05-13 13:46:16 -0400

    rendering well, images static

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const path = require('path');
 const bodyParser = require('body-parser')
 const cors = require('cors');
 const morgan = require('morgan');
+const { createProxyMiddleware } = require('http-proxy-middleware')
+var proxy = require('express-http-proxy');
 
 const app = express();
 const port = 3000;
@@ -14,9 +16,18 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.put('/moreHomes', (req, res) => {
-  console.log('init put request recieved')
-  console.log(req.body.id)
-})
+// app.use('/', proxy('http://localhost:3001'));
+// app.use('/', proxy('http://localhost:3003'));
+// app.use('/', proxy('http://localhost:3004'));
+
+// app.use('/', createProxyMiddleware({target: 'http://localhost:3001'}))
+// app.use('/', createProxyMiddleware({target: 'http://localhost:3003'}))
+// app.use('/', createProxyMiddleware({target: 'http://localhost:3004'}))
+
+// app.get(`/moreHomes/:idx`, (req, res) => {
+//   console.log('init put request recieved')
+//   console.log(req.params.idx)
+//   // take req.params.idx -> send that to MoreHomes
+// })
 
 app.listen(port, () => console.log(`Proxy listening at port ${port}`));
